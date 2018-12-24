0c7e9c2b50b59bafc956bf61cdb905bd882acdb9
Max Weis <maxrobweis@gmail.com>
2018-12-24 04:07:27 -0600

    rom struct update, update free function, use safer calloc call

--- rom.c
+++ rom.c
@@ -1,37 +1,41 @@
 #include "rom.h"
+#include "instruction.h"
 
-bool parse_rom(FILE *rom, uint8_t **prg, uint8_t **chr) {
-    if (rom == NULL) {
+bool parse_rom(FILE *rom_file, Rom *rom) {
+    if (rom_file == NULL) {
         return false;
     }
 
     unsigned prg_length = 0;
-    fseek(rom, PRG_LEN_BYTE_LOC, SEEK_SET); // access prg length from rom header
-    fread(&prg_length, 1, 1, rom);
+    fseek(rom_file, PRG_LEN_BYTE_LOC, SEEK_SET); // access prg length from rom header
+    fread(&prg_length, 1, 1, rom_file);
     prg_length *= PRG_BLOCK_SIZE;
 
     unsigned chr_length = 0;
-    fseek(rom, CHR_LEN_BYTE_LOC, SEEK_SET); // access chr length from rom header
-    fread(&chr_length, 1, 1, rom);
+    fseek(rom_file, CHR_LEN_BYTE_LOC, SEEK_SET); // access chr length from rom header
+    fread(&chr_length, 1, 1, rom_file);
     chr_length *= CHR_BLOCK_SIZE;
 
-    *prg = (uint8_t*) malloc(prg_length * sizeof(uint8_t) + 1);
-    *chr = (uint8_t*) malloc(chr_length * sizeof(uint8_t) + 1);
+    rom->prg = (uint8_t*) calloc(prg_length + 1, sizeof(uint8_t));
+    rom->chr = (uint8_t*) calloc(chr_length + 1, sizeof(uint8_t));
 
-    fseek(rom, PRG_BLOCK_BEGIN_LOC, SEEK_SET);
+    fseek(rom_file, PRG_BLOCK_BEGIN_LOC, SEEK_SET);
     for (unsigned i = 0; i < prg_length; i++) {
-        if (!fread(*prg + i, 1, 1, rom)) { // fill in string with binary prg data
+        if (!fread(rom->prg + i, 1, 1, rom_file)) { // fill in string with binary prg data
             return false;
         }
     }
-    (*prg)[prg_length] = '\0';  // null-terminate data
+    rom->prg[prg_length] = '\0';  // null-terminate data
 
     for (unsigned i = 0; i < chr_length; i++) {
-        if (!fread(*chr + i, 1, 1, rom)) { // fill in string with binary chr data
+        if (!fread(rom->chr + i, 1, 1, rom_file)) { // fill in string with binary chr data
             return false;
         }
     }
-    (*chr)[chr_length] = '\0'; // null-terminate data
+    rom->chr[chr_length] = '\0'; // null-terminate data
+
+    rom->prg_len = prg_length;
+    rom->chr_len = chr_length;
 
     return true;
 }
@@ -39,4 +43,8 @@ bool parse_rom(FILE *rom, uint8_t **prg, uint8_t **chr) {
 void close_rom(Rom *rom) {
     free(rom->prg);
     free(rom->chr);
+    for (unsigned i = 0; i < rom->inst_amount; i++) {
+        free(rom->prg_inst[i].body);
+    }
+    free(rom->prg_inst);
 }
--- rom.h
+++ rom.h
@@ -10,11 +10,17 @@
 #define CHR_LEN_BYTE_LOC 5
 #define CHR_BLOCK_SIZE 8192
 
+typedef struct Inst Inst;
+
 typedef struct Rom {
     char *path;
     uint8_t *prg;
+    unsigned prg_len;
     uint8_t *chr;
+    unsigned chr_len;
+    Inst *prg_inst;
+    unsigned inst_amount;
 } Rom;
 
-bool parse_rom(FILE *rom, uint8_t **prg, uint8_t **chr);
+bool parse_rom(FILE *rom_file, Rom *rom_path);
 void close_rom(Rom *rom);
