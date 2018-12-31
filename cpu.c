801dd35993a3265e6b0799b6e5414850e7aeba7e
Max Weis <maxrobweis@gmail.com>
2018-12-31 03:04:31 -0600

    fix decimal mode flag, implement cpu boot state, fix stack pointer size, inline functions, implement push and pull (term for pop in 6502 lingo)

--- cpu.c
+++ cpu.c
@@ -1,17 +1,49 @@
 #include "cpu.h"
-#include "instruction.h"
 
-void set_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position, bool value) {
+// initialization of cpu upon NES boot
+CPU *new_CPU() {
+    CPU *cpu = (CPU*) calloc(1, sizeof(CPU)); // zero-initialize values
+    cpu->stack_p = 0xfd; // initialize descending, empty stack
+    cpu->status_reg = 0x0034;
+
+    return cpu;
+}
+
+inline void set_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position, bool value) {
     if (value == 0)
         cpu->status_reg &= ~(1 << bit_position);
     else
         cpu->status_reg |= (1 << bit_position);
 }
 
-bool get_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position) {
+inline bool get_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position) {
     return (cpu->status_reg & (1 << bit_position)) >> bit_position;
 }
 
-bool get_bit(uint8_t byte, unsigned pos) {
+inline bool get_bit(uint8_t byte, unsigned pos) {
     return byte & (1 << pos);
+}
+
+void stack_push(NES *nes, uint8_t value) {
+    nes->ram[nes->cpu->stack_p--] = value;
+}
+
+void stack_push16(NES *nes, uint16_t value) {
+    stack_push(nes, (uint8_t) value); // truncate for least significant byte
+    stack_push(nes, (uint8_t) (value >> 8)); // push most significant byte
+}
+
+uint8_t stack_pull(NES *nes) { // pull = pop in 6502 lingo
+    return nes->ram[++nes->cpu->stack_p];
+}
+
+uint16_t stack_pull16(NES *nes) {
+    uint8_t pull0 = stack_pull(nes);
+    uint8_t pull1 = stack_pull(nes);
+    uint16_t result = (pull0 << 8) | pull1;
+    return result;
+}
+
+uint8_t negate_byte(uint8_t byte) {
+    return (~byte) + 1;
 }
--- cpu.h
+++ cpu.h
@@ -2,15 +2,19 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "nes.h"
+#include "instruction.h"
 
 #define CPU_CLOCK 21441960
+
+typedef struct NES NES;
 
 typedef struct CPU {
     uint8_t acc_reg;    // accumulator register
     uint8_t x_reg;      // x tiling register
     uint8_t y_reg;      // y tiling register
     uint8_t status_reg; // status register [NEGATIVE | OVERFLOW | | BRK COMMAND | DECIMAL MODE (NOT USED) | IRQ DISABLE | ZERO | CARRY]
-    uint16_t stack_p;   // stack pointer
+    uint8_t stack_p;   // stack pointer
     uint16_t program_c; // program counter
 } CPU;
 
@@ -18,11 +22,18 @@ typedef enum STATUS_REG_BIT {
     CARRY = 0,
     ZERO = 1,
     IRQ_DISABLE = 2,
+    DECIMAL = 3,
     BRK = 4,
-    OVERFLOW = 6,
-    NEGATIVE = 7
+    OVERFLOW = 5,
+    NEGATIVE = 6
 } STATUS_REG_BIT;
 
+CPU *new_CPU();
 void set_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position, bool value);
 bool get_cpu_status_bit(CPU *cpu, STATUS_REG_BIT bit_position);
 bool get_bit(uint8_t byte, unsigned pos);
+void stack_push(NES *nes, uint8_t value);
+void stack_push16(NES *nes, uint16_t value);
+uint8_t stack_pull(NES *nes);
+uint16_t stack_pull16(NES *nes);
+uint8_t negate_byte(uint8_t byte);
